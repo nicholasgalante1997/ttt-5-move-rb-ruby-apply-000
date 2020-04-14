3dbe2a50f3495afef9b821438ef0a22d1dc8910a
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-14 22:46:22 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,9 @@ end
 
 def input_to_index(user_input)
   user_input = gets.strip
-  new_user_input = user_input.to_i
+  new_user_input = user_input.to_i - 1 
+  return new_user_input
 end
+  
   
   
